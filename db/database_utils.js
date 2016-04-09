4cef87022f928b8a1b6b06145ca8c7a6e520cc85
Perale Thomas <thomas.perale@openmailbox.org>
2016-04-09 15:12:58 +0200

    Added get_bar function.

--- db/database_utils.js
+++ db/database_utils.js
@@ -2,8 +2,8 @@ var sqlite3 = require('sqlite3').verbose();
 var fs = require("fs");
 
 // var file  = process.env.CLOUD_DIR + "/" + "test.db";
-var file  = "test.db";
-var creation_script_file = process.env.CLOUD_DIR + "/" + "create.sql";
+var file  = "./db/test.db";
+var creation_script_file = "create.sql";
 
 var exists = fs.existsSync(file);
 
@@ -16,14 +16,12 @@ var db = new sqlite3.Database(file);
 
 db.serialize(function () {
     if (!exists) {
-
         fs.readFile(creation_script_file, 'utf8', function (err, data) {
             if (err) {
                 return console.log(err);
             }
             db.run(data); 
         });
-
     }
 });
 
@@ -38,44 +36,29 @@ db.serialize(function () {
  *  phone_number,
  *  website,
  *  picture,
- *  creator
+ *  created_by
  * }
  */
 var insert_establishment = function (establishment, callback) {
-    // var stmt = db.prepare("INSERT INTO establishment (latitude, longitude, name, address_street, address_town, address_number, phone_number, website, picture, creation_date, created_by) VALUES (?)");
-    var command = "INSERT INTO establishment (latitude, longitude, name, address_street, address_town, address_zip, address_number, phone_number, website, picture, created_by) VALUES (";
-
-    command += establishment.latitude + ", ";
-    command += establishment.longitude + ", ";
-    command += "\"" + establishment.name + "\"" + ", ";
-    command += "\"" + establishment.address_street + "\"" + ", ";
-    command += "\"" + establishment.address_town + "\"" +  ", ";
-    command += establishment.address_zip + ", ";
-    command += establishment.address_number + ", ";
-    command += "\"" + establishment.phone_number + "\"" + ", ";
-    command += establishment.website + ", ";
-    // command += establishment.picture + ", ";
-    command += null + ", ";
-    command += "\"" + establishment.creator + "\"";
-
-    command += ")"
+    var command = "INSERT INTO establishment (latitude, longitude, name, address_street, address_town, address_zip, address_number, phone_number, website, picture, created_by)\
+                                    VALUES ($latitude, $longitude, $name, $address_street, $address_town, $address_zip, $address_number, $phone_number, $website, $picture, $created_by)";
+    var st = db.prepare(command);
 
     console.log(command);
 
-    // stmt.run(command, function (err, row) {
-    db.run(command, function (err) {
+    st.run(establishment, function (err) {
         if (err) {
             console.log("ERROR WHILE INSERTING ESTABLISHMENT : " + err); 
-            console.log(err);
+            return;
         }
-        var cmd = "SELECT id FROM establishment WHERE latitude=" + establishment.latitude + " AND longitude=" + establishment.longitude;
+        var cmd = "SELECT id FROM establishment WHERE latitude=" + establishment.$latitude + " AND longitude=" + establishment.$longitude;
         db.get(cmd, function(err, row) {
             if (err) {
                 console.log("ERROR WHILE getting id : " + err + " WITH : " + cmd); 
                 return;
             }
             console.log("ROW ID " + row.id);
-            callback(establishment, row.id); 
+            callback(row.id); 
         });
     });
 };
@@ -87,6 +70,10 @@ module.exports = {
             stmt.run("Ipsum " + i);
         }
         stmt.finalize();
+    },
+
+    get_hotel : function (id) {
+        
     },
 
     new_bar : function (latitude,
@@ -102,24 +89,28 @@ module.exports = {
                         snacks,
                         creator) 
     {
-        var bar = {
-            latitude : latitude,
-            longitude : longitude,
-            name : name,
-            address_street : address_street,
-            address_town : address_town,
-            address_number : address_number,
-            address_zip : address_zip,
-            phone_number : phone_number,
-            website : website,
-            smoker : smoker,
-            snacks : snacks,
-            creator : creator
+        var establishment = {
+            $latitude : latitude,
+            $longitude : longitude,
+            $name : name,
+            $address_street : address_street,
+            $address_town : address_town,
+            $address_number : address_number,
+            $address_zip : address_zip,
+            $phone_number : phone_number,
+            $website : website,
+            $created_by : creator
         };
 
-        insert_establishment(bar, function (obj, id) {
-            var command = "INSERT INTO bar (id, smoker_allowed, snacks) VALUES (" + id + ", " + obj.smoker + ", " + obj.snacks + ")";
-            db.run(command, function (err, row) {
+        var bar = {
+            $smoker : smoker,
+            $snacks : snacks,
+        };
+
+        insert_establishment(establishment, function (id) {
+            var command = "INSERT INTO bar (id, smoker_allowed, snacks) VALUES (" + id + ", $smoker, $snacks)";
+            var st = db.prepare(command);
+            st.run(bar, function (err) {
                 if (err) {
                     console.log("ERROR WHILE INSERTING BAR : " + err);
                     return;
@@ -128,8 +119,52 @@ module.exports = {
         });
     },
 
-    get_hotel : function (id) {
+    get_bar : function (id) {
+        var bar = {
+            latitude : null,
+            longitude : null,
+            name : null,
+            address_street : null,
+            address_town : null,
+            address_number : null,
+            address_zip : null,
+            phone_number : null,
+            website : null,
+            created_by : null,
 
+            smoker : null,
+            snacks : null
+        };
+
+        var cmd = "SELECT latitude, longitude, name, address_street, address_town, address_zip, address_number, phone_number, website, picture, created_by FROM establishment WHERE id=" + id;
+        db.get(cmd, function(err, row) {
+            if (err) {
+                console.log("ERROR WHILE getting id : " + err + " WITH : " + cmd); 
+                return;
+            }
+            bar.latitude = row.latitude;
+            bar.longitude = row.longitude;
+            bar.name = row.name;
+            bar.address_street = row.address_street;
+            bar.address_town = row.address_town;
+            bar.address_number = row.address_number;
+            bar.address_zip = row.address_zip;
+            bar.phone_number = row.phone_number;
+            bar.website = row.website;
+            bar.created_by = row.created_by;
+            console.log("OKOK");
+        });
+
+        db.get("SELECT smoker_allowed, snacks FROM bar WHERE id=" + id, function(err, row) {
+            if (err) {
+                console.log("ERROR WHILE getting id : " + err + " WITH : " + cmd); 
+                return;
+            }
+            bar.smoker = row.smoker_allowed;
+            bar.snacks = row.snacks;
+        });
+
+        return bar;
     },
 
     get_establishment_type : function (id) {
