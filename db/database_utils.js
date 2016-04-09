3e8195ea8847b9f5f773a50bacb8bb05d8b46be5
Perale Thomas <thomas.perale@openmailbox.org>
2016-04-10 00:04:26 +0200

    SET/GET methods for every establishment type.

--- db/database_utils.js
+++ db/database_utils.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 
 // var file  = process.env.CLOUD_DIR + "/" + "test.db";
 var file  = "./db/test.db";
-var creation_script_file = "create.sql";
+var creation_script_file = "./db/create.sql";
 
 var exists = fs.existsSync(file);
 
@@ -64,16 +64,217 @@ var insert_establishment = function (establishment, callback) {
 };
 
 module.exports = {
-    new_hotel : function () {
-        var stmt = db.prepare("INSERT INTO VALUES (?)");
-        for (var i = 0; i < 10; i++) {
-            stmt.run("Ipsum " + i);
-        }
-        stmt.finalize();
+    new_hotel : function (latitude,
+                          longitude,
+                          name,
+                          address_street,
+                          address_town,
+                          address_number,
+                          address_zip,
+                          phone_number,
+                          website,
+                          creator,
+                          stars,
+                          room_number,
+                          price) 
+    {
+        var establishment = {
+            $latitude : latitude,
+            $longitude : longitude,
+            $name : name,
+            $address_street : address_street,
+            $address_town : address_town,
+            $address_number : address_number,
+            $address_zip : address_zip,
+            $phone_number : phone_number,
+            $website : website,
+            $created_by : creator
+
+        };
+
+        var hotel = {
+            $stars : stars,
+            $room_number : room_number,
+            $price : price
+        };
+
+        insert_establishment(establishment, function (id) {
+            var command = "INSERT INTO hotel (id, stars, room_number, price) VALUES (" + id + ", $stars, $room_number, $price)";
+            var st = db.prepare(command);
+            st.run(bar, function (err) {
+                if (err) {
+                    console.log("ERROR WHILE INSERTING HOTEL : " + err);
+                    return;
+                }
+            });
+        });
     },
 
     get_hotel : function (id) {
-        
+        var hotel = {
+            latitude : null,
+            longitude : null,
+            name : null,
+            address_street : null,
+            address_town : null,
+            address_number : null,
+            address_zip : null,
+            phone_number : null,
+            website : null,
+            created_by : null,
+
+            stars : null,
+            room_number : null,
+            price : null
+        };
+
+        var cmd = "SELECT latitude, longitude, name, address_street, address_town, address_zip, address_number, phone_number, website, picture, created_by FROM establishment WHERE id=" + id;
+        db.get(cmd, function(err, row) {
+            if (err) {
+                console.log("ERROR WHILE getting id : " + err + " WITH : " + cmd); 
+                return;
+            } else if (!row) {
+                console.log("ID does not exist.");
+                return;            
+            }
+
+            hotel.latitude = row.latitude;
+            hotel.longitude = row.longitude;
+            hotel.name = row.name;
+            hotel.address_street = row.address_street;
+            hotel.address_town = row.address_town;
+            hotel.address_number = row.address_number;
+            hotel.address_zip = row.address_zip;
+            hotel.phone_number = row.phone_number;
+            hotel.website = row.website;
+            hotel.created_by = row.created_by;
+        });
+
+        db.get("SELECT stars, room_number, price FROM hotel WHERE id=" + id, function(err, row) {
+            if (err) {
+                console.log("ERROR WHILE getting id : " + err + " WITH : " + cmd); 
+                return;
+            } else if (!row) {
+                console.log("ID does not exist.");
+                return;            
+            }
+            bar.smoker = row.smoker_allowed;
+            bar.snacks = row.snacks;
+        });
+
+        return bar;
+    },
+
+    new_restaurant : function (latitude,
+                               longitude,
+                               name,
+                               address_street,
+                               address_town,
+                               address_number,
+                               address_zip,
+                               phone_number,
+                               website,
+                               creator,
+                               price,
+                               seat_number,
+                               takeaway,
+                               delivery,
+                               timetable) 
+    {
+        var establishment = {
+            $latitude : latitude,
+            $longitude : longitude,
+            $name : name,
+            $address_street : address_street,
+            $address_town : address_town,
+            $address_number : address_number,
+            $address_zip : address_zip,
+            $phone_number : phone_number,
+            $website : website,
+            $created_by : creator
+
+        };
+
+        var restaurant = {
+            $price : price,
+            $seat_number : seat_number,
+            $takeaway : takeaway,
+            $delivery : delivery,
+            $timetable : timetable
+        };
+
+        insert_establishment(establishment, function (id) {
+            var command = "INSERT INTO restaurant (id, price, seat_number, takeaway, delivery, timetable) VALUES (" + id + ", $price, $seat_number, $takeaway, $delivery, $timetable)";
+            var st = db.prepare(command);
+            st.run(bar, function (err) {
+                if (err) {
+                    console.log("ERROR WHILE INSERTING BAR : " + err);
+                    return;
+                }
+            });
+        });
+    },
+
+    get_restaurant : function (id) {
+        var restaurant = {
+            latitude : null,
+            longitude : null,
+            name : null,
+            address_street : null,
+            address_town : null,
+            address_number : null,
+            address_zip : null,
+            phone_number : null,
+            website : null,
+            created_by : null,
+
+            price : null,
+            seat_number : null,
+            takeaway : null,
+            delivery : null,
+            timetable : null
+        };
+
+        var cmd = "SELECT latitude, longitude, name, address_street, address_town, address_zip, address_number, phone_number, website, picture, created_by FROM establishment WHERE id=" + id;
+        db.get(cmd, function(err, row) {
+            if (err) {
+                console.log("ERROR WHILE getting id : " + err + " WITH : " + cmd); 
+                return;
+            } else if (!row) {
+                console.log("ID does not exist.");
+                return;            
+            }
+
+            restaurant.latitude = row.latitude;
+            restaurant.longitude = row.longitude;
+            restaurant.name = row.name;
+            restaurant.address_street = row.address_street;
+            restaurant.address_town = row.address_town;
+            restaurant.address_number = row.address_number;
+            restaurant.address_zip = row.address_zip;
+            restaurant.phone_number = row.phone_number;
+            restaurant.website = row.website;
+            restaurant.created_by = row.created_by;
+            console.log("OKOK");
+        });
+
+        db.get("SELECT price, seat_number, takeaway, delivery, timetable FROM restaurant WHERE id=" + id, function(err, row) {
+            if (err) {
+                console.log("ERROR WHILE getting id : " + err + " WITH : " + cmd); 
+                return;
+            } else if (!row) {
+                console.log("ID does not exist.");
+                return;            
+            }
+
+            restaurant.price = row.price;
+            restaurant.seat_number = row.seat_number;
+            restaurant.takeaway = row.takeaway;
+            restaurant.delivery = row.delivery;
+            restaurant.timetable = row.timetable;
+        });
+
+        return bar;
     },
 
     new_bar : function (latitude,
@@ -141,7 +342,11 @@ module.exports = {
             if (err) {
                 console.log("ERROR WHILE getting id : " + err + " WITH : " + cmd); 
                 return;
+            } else if (!row) {
+                console.log("ID does not exist.");
+                return;            
             }
+
             bar.latitude = row.latitude;
             bar.longitude = row.longitude;
             bar.name = row.name;
@@ -159,6 +364,9 @@ module.exports = {
             if (err) {
                 console.log("ERROR WHILE getting id : " + err + " WITH : " + cmd); 
                 return;
+            } else if (!row) {
+                console.log("ID does not exist.");
+                return;            
             }
             bar.smoker = row.smoker_allowed;
             bar.snacks = row.snacks;
@@ -167,18 +375,42 @@ module.exports = {
         return bar;
     },
 
-    get_establishment_type : function (id) {
-        var result = {};
+    get_establishment_type : function (id, callback) {
+        db.get("SELECT id FROM bar WHERE id=" + id, function(err, row) {
+            if (err) {
+                console.log(err); 
+                return;
+            } else if (!row) {
+                return;
+            }
+            callback("bar");
+        });
 
-        db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-            console.log(row.id + ": " + row.info);
+        db.get("SELECT id FROM restaurant WHERE id=" + id, function(err, row) {
+            if (err) {
+                console.log(err); 
+                return;
+            } else if (!row) {
+                return;
+            }
+            callback("restaurant");
+        });
+
+        db.get("SELECT id FROM hotel WHERE id=" + id, function(err, row) {
+            if (err) {
+                console.log(err); 
+                return;
+            } else if (!row) {
+                return;
+            }
+            callback("hotel");
         });
     },
 
     search_establishment : function (name) {
         var result = [];
 
-        db.each("SELECT id, name FROM establishment WHERE name LIKE '%" + name + "%';", function(err, row) {
+        db.each("SELECT id, name FROM establishment WHERE name LIKE '%" + name + "%'", function(err, row) {
             result.push({ 
                 id : row.id,
                 name : row.name
@@ -186,7 +418,21 @@ module.exports = {
         });
     },
 
-    get_establishment_locations : function () {
-        return [{latitude : 50.8491, longitude : 4.3659}, {latitude : 50.8391, longitude : 4.3750}, {latitude : 50.8500, longitude : 4.3650}];
+    get_establishment_locations : function (callback) {
+        var result = [];
+
+        db.each("SELECT id, name, latitude, longitude FROM establishment", function(err, row) {
+            result.push({ 
+                latitude : row.latitude,
+                longitude : row.longitude,
+                name : row.name,
+                id : row.id
+            });
+        }, function (err, row) {
+            if (err) {
+                console.log(err);            
+            }
+            callback(result);
+        });
     }
 };
