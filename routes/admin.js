b5c871a3e6507f509cd1ec9cf8f7979250ebf7c5
Perale Thomas <thomas.perale@openmailbox.org>
2016-05-02 14:35:22 +0200

    Changed enctype to use formidable.

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');                                                                                                                                        
 
 router.get('/add', function (req, res) {
     if ((!req.user) || (!req.user.admin)) {
@@ -12,8 +13,34 @@ router.get('/add', function (req, res) {
     });
 });
 
-router.post('/add', function (req, res) {
+router.post('/add/restaurant', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, file) {
+        if (err) {
+            console.log("Error in form : " + err);
+            return res.redirect(303, '/error');
+        }
+    });
+});
 
+router.post('/add/bar', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, file) {
+        if (err) {
+            console.log("Error in form : " + err);
+            return res.redirect(303, '/error');
+        }
+    });
+});
+
+router.post('/add/hotel', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, file) {
+        if (err) {
+            console.log("Error in form : " + err);
+            return res.redirect(303, '/error');
+        }
+    });
 });
 
 module.exports = router;
