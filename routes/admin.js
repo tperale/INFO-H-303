2da685ac155481a039fb9135298743c838a4e8d5
Perale Thomas <thomas.perale@openmailbox.org>
2016-05-03 17:23:44 +0200

    Add admin interface.

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var formidable = require('formidable');                                                                                                                                        
 
+var Restaurant = require('../db/restaurant_db_utils.js');
+var Bar = require('../db/bar_db_utils.js');
+var Hotel = require('../db/hotel_db_utils.js');
+
 router.get('/add', function (req, res) {
     if ((!req.user) || (!req.user.admin)) {
         return res.redirect(303, '404');
@@ -20,6 +24,13 @@ router.post('/add/restaurant', function (req, res) {
             console.log("Error in form : " + err);
             return res.redirect(303, '/error');
         }
+
+        fields.takeaway = ((fields.takeaway == "on") ? 1 : 0);
+        fields.delivery = ((fields.delivery == "on") ? 1 : 0);
+
+        Restaurant.new_restaurant(fields, function (id) {
+            res.redirect('back');
+        });
     });
 });
 
@@ -30,6 +41,13 @@ router.post('/add/bar', function (req, res) {
             console.log("Error in form : " + err);
             return res.redirect(303, '/error');
         }
+
+        fields.smokers = ((fields.smokers == "on") ? 1 : 0);
+        fields.snacks = ((fields.snacks == "on") ? 1 : 0);
+
+        Bar.new_bar(fields, function (id) {
+            res.redirect('back');
+        });
     });
 });
 
@@ -40,6 +58,11 @@ router.post('/add/hotel', function (req, res) {
             console.log("Error in form : " + err);
             return res.redirect(303, '/error');
         }
+
+        Hotel.new_hotel(fields, function (id) {
+            res.redirect('back');
+        });
+
     });
 });
 
